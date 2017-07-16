bc3dac55bde86b9c70807f5aa0da0ba4c67cdc54
junshao <joshuaton@gmail.com>
2017-07-16 22:14:02 +0800

    【优化】验证码出现时间优化

--- Flipped/Utils/FLToast.m
+++ Flipped/Utils/FLToast.m
@@ -16,7 +16,7 @@
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:[FLBaseViewController currentViewController].view animated:YES];
     hud.mode = MBProgressHUDModeText;
     hud.label.text = text;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [MBProgressHUD hideHUDForView:[FLBaseViewController currentViewController].view animated:YES];
     });
 }
