c856bc3b2d971b93e474bf40f6ebb48eb30c7caa
junshao <joshuaton@gmail.com>
2017-07-16 22:00:32 +0800

    【新增】封装了下toast

---
+++ Flipped/Utils/FLToast.h
@@ -0,0 +1,15 @@
+//
+//  FLToast.h
+//  Flipped
+//
+//  Created by ShaoJun on 2017/7/16.
+//  Copyright © 2017年 junshao. All rights reserved.
+//
+
+#import "MBProgressHUD.h"
+
+@interface FLToast : NSObject
+
++(void)showToast:(NSString *)text;
+
+@end
---
+++ Flipped/Utils/FLToast.m
@@ -0,0 +1,24 @@
+//
+//  FLToast.m
+//  Flipped
+//
+//  Created by ShaoJun on 2017/7/16.
+//  Copyright © 2017年 junshao. All rights reserved.
+//
+
+#import "FLToast.h"
+#import "FLBaseViewController.h"
+
+@implementation FLToast
+
++(void)showToast:(NSString *)text{
+    
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:[FLBaseViewController currentViewController].view animated:YES];
+    hud.mode = MBProgressHUDModeText;
+    hud.label.text = text;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [MBProgressHUD hideHUDForView:[FLBaseViewController currentViewController].view animated:YES];
+    });
+}
+
+@end
