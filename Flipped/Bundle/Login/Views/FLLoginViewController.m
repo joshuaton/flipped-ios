dc180ef1e94f64909a848c7b459c74f90a3d2262
junshao <joshuaton@gmail.com>
2017-07-16 22:32:51 +0800

    【新增】验证码倒计时功能，还有待优化

--- Flipped/Bundle/Login/Views/FLLoginViewController.m
+++ Flipped/Bundle/Login/Views/FLLoginViewController.m
@@ -19,6 +19,7 @@
 @property (nonatomic, strong) UIButton *getVertifyCodeButton;
 @property (nonatomic, strong) UIButton *loginButton;
 @property (nonatomic, strong) UILabel *helpLabel;
+@property (nonatomic, assign) NSInteger time;
 
 @end
 
@@ -71,10 +72,66 @@
     }];
 }
 
--(void)getVertifyBtnClick{
+-(void)getVertifyBtnClick:(UIButton *)sender{
     [FLUserService getVertifyCodeWithWithPhoneNum:self.phoneNumTextField.text successBlock:^{
         
         [FLToast showToast:@"验证码已经发送到手机"];
+        
+        __block int timeout=60; //倒计时时间
+        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+        dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
+        dispatch_source_set_event_handler(_timer, ^{
+            
+            if(timeout<=0){ //倒计时结束，关闭
+                
+                dispatch_source_cancel(_timer);
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    //设置界面的按钮显示 根据自己需求设置
+                    [sender setTitle:@"获取验证码" forState:UIControlStateNormal];
+                    sender.backgroundColor = [UIColor whiteColor];
+                    sender.userInteractionEnabled = YES;
+                });
+            }else{
+                //            int minutes = timeout / 60;
+                int seconds = timeout % 59;
+                __block NSString *strTime = [NSString stringWithFormat:@"%.2d", seconds];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    
+                    CFAbsoluteTime start =CFAbsoluteTimeGetCurrent();
+                    
+                    if((start -_time) >1.5&& [strTime intValue] >1&& [strTime intValue] <30) {
+                        
+                        int num = (int)ceil(start -_time);
+                        
+                        timeout = timeout - num +1;
+                        
+                        strTime = [NSString stringWithFormat:@"%zd", [strTime intValue] - num];
+                        
+                        timeout--;
+                        
+                        if(timeout <0) {
+                            
+                            return;
+                            
+                        }
+                        
+                    }
+                    
+                    _time=CFAbsoluteTimeGetCurrent();
+                    
+                    //设置界面的按钮显示 根据自己需求设置
+                    NSLog(@"____%@",strTime);
+                    [sender setTitle:[NSString stringWithFormat:@"%@",strTime] forState:UIControlStateNormal];
+                    sender.backgroundColor = [UIColor grayColor];
+                    sender.userInteractionEnabled = NO;
+                });
+                timeout--;
+            }
+        });
+        
+        dispatch_resume(_timer);
+        
     } failBlock:^(NSError *error) {
         
         
@@ -138,7 +195,7 @@
         [_getVertifyCodeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         _getVertifyCodeButton.layer.borderWidth = 1;
         _getVertifyCodeButton.layer.borderColor = [UIColor blackColor].CGColor;
-        [_getVertifyCodeButton addTarget:self action:@selector(getVertifyBtnClick) forControlEvents:UIControlEventTouchUpInside];
+        [_getVertifyCodeButton addTarget:self action:@selector(getVertifyBtnClick:) forControlEvents:UIControlEventTouchUpInside];
         [self.view addSubview:_getVertifyCodeButton];
     }
     return _getVertifyCodeButton;
