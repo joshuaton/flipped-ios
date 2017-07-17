0a3d529a5d56f9f85cb8474ad957f15bc7c10c92
junshao <joshuaton@gmail.com>
2017-07-17 11:24:23 +0800

    【优化】获取验证码的按钮倒计时

--- Flipped/Bundle/Login/Views/FLLoginViewController.m
+++ Flipped/Bundle/Login/Views/FLLoginViewController.m
@@ -64,6 +64,46 @@
     }];
 }
 
+#pragma mark - private
+
+// 开启倒计时效果
+-(void)openCountdown{
+    
+    __block NSInteger time = 59; //倒计时时间
+    
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
+    
+    dispatch_source_set_event_handler(_timer, ^{
+        
+        if(time <= 0){ //倒计时结束，关闭
+            
+            dispatch_source_cancel(_timer);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                //设置按钮的样式
+                [self.getVertifyCodeButton setTitle:@"获取验证码" forState:UIControlStateNormal];
+                [self.getVertifyCodeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+                self.getVertifyCodeButton.userInteractionEnabled = YES;
+            });
+            
+        }else{
+            
+            int seconds = time % 60;
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                //设置按钮显示读秒效果
+                [self.getVertifyCodeButton setTitle:[NSString stringWithFormat:@"重新发送(%.2d)", seconds] forState:UIControlStateNormal];
+                [self.getVertifyCodeButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+                self.getVertifyCodeButton.userInteractionEnabled = NO;
+            });
+            time--;
+        }
+    });
+    dispatch_resume(_timer);
+}
+
 #pragma mark - action
 
 -(void)closeBtnClick{
@@ -77,60 +117,7 @@
         
         [FLToast showToast:@"验证码已经发送到手机"];
         
-        __block int timeout=60; //倒计时时间
-        dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-        dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
-        dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
-        dispatch_source_set_event_handler(_timer, ^{
-            
-            if(timeout<=0){ //倒计时结束，关闭
-                
-                dispatch_source_cancel(_timer);
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    //设置界面的按钮显示 根据自己需求设置
-                    [sender setTitle:@"获取验证码" forState:UIControlStateNormal];
-                    sender.backgroundColor = [UIColor whiteColor];
-                    sender.userInteractionEnabled = YES;
-                });
-            }else{
-                //            int minutes = timeout / 60;
-                int seconds = timeout % 59;
-                __block NSString *strTime = [NSString stringWithFormat:@"%.2d", seconds];
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    
-                    CFAbsoluteTime start =CFAbsoluteTimeGetCurrent();
-                    
-                    if((start -_time) >1.5&& [strTime intValue] >1&& [strTime intValue] <30) {
-                        
-                        int num = (int)ceil(start -_time);
-                        
-                        timeout = timeout - num +1;
-                        
-                        strTime = [NSString stringWithFormat:@"%zd", [strTime intValue] - num];
-                        
-                        timeout--;
-                        
-                        if(timeout <0) {
-                            
-                            return;
-                            
-                        }
-                        
-                    }
-                    
-                    _time=CFAbsoluteTimeGetCurrent();
-                    
-                    //设置界面的按钮显示 根据自己需求设置
-                    NSLog(@"____%@",strTime);
-                    [sender setTitle:[NSString stringWithFormat:@"%@",strTime] forState:UIControlStateNormal];
-                    sender.backgroundColor = [UIColor grayColor];
-                    sender.userInteractionEnabled = NO;
-                });
-                timeout--;
-            }
-        });
-        
-        dispatch_resume(_timer);
+        [self openCountdown];
         
     } failBlock:^(NSError *error) {
         
